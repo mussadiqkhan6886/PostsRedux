d3529883c2e4767d7bbf324c17f611ad192dd3ad
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-07-12 21:25:12 +0500

    added tailwind

--- src/features/Posts/PostsExcerpt.jsx
+++ src/features/Posts/PostsExcerpt.jsx
@@ -7,7 +7,7 @@ const PostsExcerpt = ({ post }) => {
         <article>
             <h3>{post.title}</h3>
             <p>{post.body.substring(0, 100)}</p>
-            <p className="postCredit">
+            <p>
                 <PostAuthor userId={post.userId} />
                 <TimeAgo timestamp={post.date} />
             </p>
